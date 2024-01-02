2a5e6781732d98fded4c9d60196e8c91d2119dc5
vaishnavi <87173081+VaishnaviLatey21@users.noreply.github.com>
2024-01-02 22:02:12 +0530

    header commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header';
+
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Header />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import logo from "../Images/logo-d-plus.svg";
+import { HiHome, HiSearch, HiOutlinePlusSm } from "react-icons/hi";
+import { HiOutlineFilm, HiOutlineGlobeAlt, HiMiniUserCircle, HiMiniEllipsisVertical } from "react-icons/hi2";
+
+
+function Header() {
+    const [showMenu, setShowMenu] = useState(false)
+
+    const menu = [
+        {
+            name: "Home",
+            icon: <HiHome />
+        },
+        {
+            name: "Search",
+            icon: <HiSearch />
+        },
+        {
+            name: "WatchList",
+            icon: <HiOutlinePlusSm />
+        },
+        {
+            name: "Movies",
+            icon: <HiOutlineFilm />
+        },
+        {
+            name: "Sports",
+            icon: <HiOutlineGlobeAlt />
+        }
+    ]
+
+
+    return (
+        <div className='flex items-center gap-8 p-5 justify-between'>
+            <img src={logo} className='w-[80px] md:w-[100px] object-cover ' />
+            <div className='text-white hidden md:flex items-center gap-8'>
+                {menu.map((item, index) => (
+                    <div className='flex items-center gap-2 text-[20px] font-semibold hover:underline underline-offset-8'>
+                        <p>{item.icon}</p>
+                        <h2>{item.name}</h2>
+                    </div>
+                ))}
+            </div>
+            <div className='text-white flex items-center md:hidden gap-8'>
+                {menu.map((item, index) => index < 3 && (
+                    <div className='flex items-center gap-2 text-[20px] font-semibold hover:underline underline-offset-8'>
+                        <p>{item.icon}</p>
+                        <h2>{item.name}</h2>
+                    </div>
+                ))}
+                <div className='md:hidden cursor-pointer text-[25px] pt-1' onClick={() => setShowMenu(!showMenu)}>
+                    <HiMiniEllipsisVertical />
+                    {showMenu ? <div className='absolute mt-3 bg-gray-950 border-[1px] border-gray-700 p-3 py-4'>
+                        {menu.map((item, index) => index >= 3 && (
+                            <div className='flex items-center gap-2 text-[20px] font-semibold hover:underline underline-offset-8'>
+                                <p>{item.icon}</p>
+                                <h2>{item.name}</h2>
+                            </div>
+                        ))}
+                    </div> : null}
+                </div>
+            </div>
+
+            <HiMiniUserCircle className='text-white text-[45px] mr-3 cursor-pointer' />
+
+
+        </div>
+    )
+}
+
+export default Header
